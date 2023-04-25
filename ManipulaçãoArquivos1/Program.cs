72752e36b633d67e487ccdda7c7345b70726b5ce
Arthur de Lima Araujo <56854104+Arthur-1702@users.noreply.github.com>
2023-04-25 10:18:54 -0300

    Add files via upload

---
+++ ManipulaçãoArquivos1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ManipulaçãoArquivos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //StreamReader x;
+            //StreamWriter x;
+
+            string path = "C:\\Users\\arthur.araujo\\OneDrive - Kurier Tecnologia\\Documentos\\AtividadesVB\\ManipulaçãoArquivos1\\Arquivos\\arquivo01.txt";
+            
+            //File.Delete(Caminho);
+
+            //System.Diagnostics.Process.Start("notepad", path);
+           
+            /* 
+            x = File.OpenText(path);
+
+            while (x.EndOfStream != true)
+            {
+                Console.WriteLine(x.ReadLine());
+            }
+            */
+           
+
+//      Para escrever num arquivo existente
+            //x = File.AppendText(path);
+
+            //x.WriteLine();
+            //x.WriteLine("_____________________");
+            //x.WriteLine("Endereço: Rua Abcde, 193 - Centro");
+            //x.WriteLine("_____________________");
+
+//      Para escrever um novo arquivo
+//            x = File.CreateText(path);
+//            Console.ReadKey();
+
+    //        x.WriteLine("Nota Fiscal");
+  //          x.WriteLine();
+//            x.WriteLine("_______________________________");
+//            x.WriteLine();
+  //          x.WriteLine("Empresa: NET Ltda");
+    //        x.WriteLine("Quantia: R$100,00");
+      //      x.WriteLine("Descrição: Curso");
+        //    x.WriteLine("Local: Recife - PE");
+          //  x.WriteLine("Data/Hora: 14/04 10:55:48");
+            //x.WriteLine();
+//            x.WriteLine("_______________________________");
+  //          x.WriteLine("Cliente: Arthur");
+    //        x.WriteLine("CPF: 123.456.321-15");
+      //      x.WriteLine();
+
+            //x.Close(); 
+
+            Console.ReadKey();
+
+        }
+    }
+}
